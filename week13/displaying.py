9526ca5e563bb7518cf67bf39d9d86324874ed35
nimax <nimax29@gmail.com>
2022-12-07 10:42:29 -0700

    week 13

---
+++ week13/displaying.py
@@ -0,0 +1,14 @@
+
+message = input("What is your message?: ")
+
+def uppercase(_message):
+    return _message.upper()
+
+def lowerrcase(_message):
+    return _message.lower()
+
+print()
+print(message)
+print(uppercase(message))
+print(lowerrcase(message))
+print()
---
+++ week13/preparation.py
@@ -0,0 +1,9 @@
+
+# First define the function
+def print_message():
+    print("Hello CSE 110 World!")
+
+# Call the function now
+print()
+print_message()
+print()
